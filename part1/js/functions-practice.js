26c668a6bef77a3841f37adffbcc0d612febebf3
GarrettJMU <garretthughes3@gmail.com>
2019-10-08 16:10:31 -0700

    almost done with part 1

--- part1/js/functions-practice.js
+++ part1/js/functions-practice.js
@@ -1,9 +1,19 @@
 //STEP 1
+function halfNumber(number) {
+  console.log(`Half of ${number} is ${number / 2}.`)
+}
 
 //STEP 2
+function squareNumber(number) {
+  console.log(`The result of squaring the number ${number} is ${number * number}.`)
+}
 
 //STEP 3
-
+function percentOf(number, wholeValue) {
+  let percentage = (number / wholeValue) * 100
+  console.log(`${number} is ${percentage}% of ${wholeValue}`)
+}
 //STEP 4
+function findModulus()
 
 //STEP 5
